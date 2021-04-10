a0385e0f2d3a3f7837fe8d116132d854aba9dbdc
Semyon-prog <81463505+Semyon-prog@users.noreply.github.com>
2021-04-10 15:33:42 +0700

    Update пингпон.py

--- пингпон.py
+++ пингпон.py
@@ -1,6 +1,6 @@
 from pygame import *
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, wigth, height, player_speed):
+    def __init__(self, player_image, player_x, player_y, player_speed, wigth, height):
         super().__init__()
         self.image = transform.scale(image.load(player_image), (wigth, height))
         self.speed = player_speed
